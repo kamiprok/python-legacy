07b3afdd584288cea953d804c82ad8ff0688cba2
kamiprok <kamiprok@gmail.com>
2019-02-07 17:19:16 +0100

    updating heads.py

--- heads.py
+++ heads.py
@@ -1,20 +1,27 @@
 import random
 
-user_input = input('Heads or Tails? \n')
+def heads():
+    user_input = input('Heads or Tails? \n')
 
-if 'head' in user_input.lower():
-  print('Your pick: Heads')
-elif 'tail' in user_input.lower():
-  print('Your pick: Tails')
-else:
-  print('problem')
+    if 'head' in user_input.lower() and 'tail' in user_input.lower():
+        print("It doesn't work like that...")
+        exit()
+    elif 'head' in user_input.lower() or user_input.lower() == 'h':
+        print('Your pick: Heads')
+    elif 'tail' in user_input.lower() or user_input.lower() == 't':
+        print('Your pick: Tails')
+    else:
+        print("You didn't pick any!")
 
-pick = random.choice(['Tails', 'Heads'])
-cpu_input = str(pick)
+    pick = random.choice(['Tails', 'Heads'])
+    cpu_input = str(pick)
 
-print('CPU pick: ', cpu_input)
+    print('CPU pick: ', cpu_input)
 
-if user_input.lower() in cpu_input.lower():
-  print('You guessed it!')
-else:
-  print('Maybe next time :(')
+    if user_input.lower() in cpu_input.lower():
+        print('You guessed it!')
+    else:
+        print('Maybe next time :(')
+
+
+heads()
