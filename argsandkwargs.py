983115392a43a2a972648d12e381b8eb88c5cd21
kamiprok <kamiprok@gmail.com>
2019-05-21 13:48:10 +0200

    first commit args and kwargs

---
+++ argsandkwargs.py
@@ -0,0 +1,13 @@
+def funkcja_args(*args):
+    print(*args)
+
+
+funkcja_args('coś', 1, 5)
+
+
+def funkcja_kwargs(**kwargs):
+    for key, value in kwargs.items():
+        print("{0} = {1}".format(key, value))
+
+
+funkcja_kwargs(name="John", surname="Smith")
