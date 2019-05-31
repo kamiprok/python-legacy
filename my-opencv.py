3063609df1ea9b7852cae2ddc85ee76af2d8b3f1
kamiprok <kamiprok@gmail.com>
2019-05-31 16:35:00 +0200

    opencv rotation

--- my-opencv.py
+++ my-opencv.py
@@ -4,6 +4,18 @@ img = cv2.imread('pyimg.jpg')
 
 print(type(img))
 
-cv2.imshow('Original Image', img)
-cv2.waitKey()
+# cv2.imshow('Original Image', img)
+# cv2.waitKey()
 
+print(img.shape[0:2])
+
+width = img.shape[0]
+height = img.shape[1]
+
+# (center point, angle, scale)
+rotationMatrix = cv2.getRotationMatrix2D((250, 650), 30, .5)
+
+rotatedImage = cv2.warpAffine(img, rotationMatrix, (width, height))
+
+cv2.imshow('Rotated Image', rotatedImage)
+cv2.waitKey()
