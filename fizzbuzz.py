198240fa6919855cd032a17b30d5ee981102defd
Prokopiuk <kamil.prokopiuk@igt.com>
2019-08-02 11:00:30 +0200

    small fix for fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,11 @@
 def fizzbuzz(x, y):
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print(i, x+y)
+            print(x+y)
         elif i % 3 == 0:
-            print(i, x)
+            print(x)
         elif i % 5 == 0:
-            print(i, y)
+            print(y)
         else:
             print(i)
 
