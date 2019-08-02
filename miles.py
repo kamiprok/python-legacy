54e94ce5e535df402a0de8858f82d9af57d7c950
kamiprok <kamiprok@gmail.com>
2019-08-02 11:15:38 +0200

    added extra responese

--- miles.py
+++ miles.py
@@ -5,6 +5,12 @@ speed = int(input('Enter a number: '))
 if 0 < speed < 1001:
     print(speed, 'miles is around', round(speed/0.6124), 'kilometers')
     print(speed, 'kilometers is around', round(speed * 0.6124), 'miles')
+elif speed == 0:
+    print('You are not moving anyway...')
+elif speed < 0:
+    print("You can't go that slow...")
+elif speed > 1001:
+    print('You are going too fast!')
 else:
     print('error')
 
