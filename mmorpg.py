880d8354d7962548b17a0f718f84e041f271547e
kamiprok <kamiprok@gmail.com>
2019-08-02 14:11:31 +0200

    send login and pass from console

--- mmorpg.py
+++ mmorpg.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
+import getpass
 
+login = input('Login: ')
+pwd = getpass.getpass('Password: ')
 browser = webdriver.Chrome('C:/Users/kprokopiuk/Downloads/chromedriver.exe')
 browser.get('https://mmorpg.org.pl')
 loguj = browser.find_element_by_class_name('btn-login')
 loguj.click()
 userElem = browser.find_element_by_id('user_login')
-userElem.send_keys('login')  # admn no here
+userElem.send_keys(login)  # admn no here
 passwordElem = browser.find_element_by_id('user_password')
-passwordElem.send_keys('password')  # password here
+passwordElem.send_keys(pwd)  # password here
 loginElem = browser.find_element_by_xpath("//input[@value='Zaloguj się']")
 loginElem.submit()
 
