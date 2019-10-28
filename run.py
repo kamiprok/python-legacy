5f240967974e8a18c87abfa3420939fa6dc3bf40
kamiprok <kamiprok@gmail.com>
2019-10-28 11:44:24 +0100

    bs4 project

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
-import webbrowser, time
+import webbrowser
+import time
+from bs4 import BeautifulSoup
 
-webbrowser.get('"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe" %s')
-while True:
-    webbrowser.get('"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe" %s').open('google.com', new=0)
-    time.sleep(5)
-    webbrowser.open('bing.com', new=0)
+browser = webbrowser.get('"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe" %s')
+# while True:
+#     webbrowser.get('"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe" %s').open('google.com', new=0)
+#     time.sleep(5)
+#     webbrowser.open('bing.com', new=0)
 
+# browser.open('google.com', new=0)
+#
+# browser.open('reddit.com', new=0)
 
+html_doc = 'kamiprok.github.io'
+
+soup = BeautifulSoup(html_doc, 'lxml')
+
+print(soup.prettify())
+print(BeautifulSoup('kamiprok', features='lxml'))
