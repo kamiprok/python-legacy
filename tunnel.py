c0682faf7151ef6e61ebeb17eb351bfa64b0056c
kamiprok <kamiprok@gmail.com>
2019-12-04 16:46:20 +0100

    auto login for putty session early alpha

--- tunnel.py
+++ tunnel.py
@@ -2,6 +2,7 @@
 import subprocess
 import getpass
 
+host = input('host: ')
 usr = input('usr: ')
 pw = getpass.getpass(prompt='pw: ')
-pid = subprocess.Popen(f"putty.exe {usr}@ibm251 -pw {pw}").pid
+pid = subprocess.Popen(f"putty.exe {usr}@{host} -pw {pw}").pid
