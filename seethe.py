77e3ce59edcb92b34421f5ec7c7e2cdef3fa728e
kamiprok <kamiprok@gmail.com>
2020-02-03 11:20:24 +0100

    seethe fix

--- seethe.py
+++ seethe.py
@@ -3,7 +3,7 @@ import random
 
 os.system('cls')
 
-list = ['SEETHE', 'COPE', 'DILATE', 'INCEL', 'HAVE SEX', 'RENT FREE', 'YIKES', 'OBSESSED']
+list = ['SEETHE', 'COPE', 'DILATE', 'INCEL', 'HAVE SEX', 'RENT FREE', 'YIKES', 'OBSESSED', 'HANDHELD']
 
 while True:
     x = input(random.choice(list))
